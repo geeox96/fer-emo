a6bbdabcf44a659fe096f615db97e4919dfc937e
Geo <geeox96@gmail.com>
2020-07-10 17:08:30 -0300

    up

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
-const expressip = require('express-ip');
 const requestIp = require('request-ip');
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 app.use(cors());
-app.use(expressip().getIpInfoMiddleware);
 
 app.get('/', function (req, res) {
   const clientIp = requestIp.getClientIp(req); 
-  const ipInfo = req.ipInfo;
-  var message = `hum, ${clientIp}          ${ipInfo.city}, ${ipInfo.country}, ip: ${ipInfo.ip}`;
-  res.send(message);
+  res.send(clientIp);
 });
 
 app.listen(process.env.PORT || 4000);
